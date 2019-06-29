1c8150e7d032e7280a19c2653248ec87671ae612
IsaacBernardes <51682756+IsaacBernardes@users.noreply.github.com>
2019-06-29 15:35:23 -0300

    Update by Isaac 29/06/2019 15:34
    -Ajuste no metodo adicionarFuncionario
    -Ajuste no metodo removerFuncionario
    -Adicionado Overloading (buscaFuncionario(int RG) e buscaFuncionario(String nomeB)

--- dados/RepositorioFuncionario.java
+++ dados/RepositorioFuncionario.java
@@ -1,41 +1,64 @@
 package dados;
 
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 import bases.Funcionario;
+import bases.Cargo;
 import negocio.Gestao;
 
 public class RepositorioFuncionario 
 {
-	ArrayList<Funcionario> Funcionarios = new ArrayList<Funcionario>();
+	private static ArrayList<Funcionario> Funcionarios = new ArrayList<Funcionario>();
+	Gestao gerencia = new Gestao();
 	
-	public void adicionarFuncionario(Funcionario novoF)
+	public void adicionarFuncionario(String nome, int RG, Cargo cargo)
 	{
-		if(Funcionarios.contains(novoF) == false)
+		Funcionario novoF = new Funcionario(nome, RG, cargo);
+		boolean jaCadastrado = false;
+		
+		for(Funcionario a: Funcionarios)
+		{
+			if(novoF.equals(a))
+				jaCadastrado = true;
+		}
+		
+		if(!jaCadastrado)
 		{
 			Funcionarios.add(novoF);
-			Gestao gerencia = new Gestao();
 			gerencia.CadastrarFuncionario(novoF);
+			JOptionPane.showMessageDialog(null, "Funcionario cadastrado com sucesso", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
 			
 		}
 		
 		else
 		{
-			//essa parte eh gui tb
-			System.out.println("O Fucionario ja existe");
+			JOptionPane.showMessageDialog(null, "Funcionario já está cadastrado", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
 	public void removerFuncionario(Funcionario funcionarioR)
 	{
-		if(Funcionarios.contains(funcionarioR))
+		boolean Cadastrado = false;
+		Funcionario serRemovido = null;
+		
+		for(Funcionario a: Funcionarios)
 		{
-			Funcionarios.remove(funcionarioR);
+			if(funcionarioR.equals(a)) {
+				Cadastrado = true;
+				serRemovido = a;
+			}
+		}
+		
+		if(Cadastrado)
+		{
+			Funcionarios.remove(serRemovido);
 		}
 		
 		else
 		{
-			//essa parte eh gui tb
-			System.out.println("O Fucionario nao existe");
+			JOptionPane.showMessageDialog(null, "Funcionário não existe", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
@@ -52,6 +75,32 @@ public class RepositorioFuncionario
 		return null;
 	}
 	
+	public Funcionario buscaFuncionario(int RG)
+	{
+		for(Funcionario a: Funcionarios)
+		{
+			if(a.getID() == RG)
+			{
+				return a;
+			}
+		}
+		
+		return null;
+	}
+	
+	public String toString()
+	{
+		String text = "-------------------------------------------------------------";
+		
+		for(Funcionario a: Funcionarios) {
+			text += "\nNome: " + a.getNome();
+			text += "\nRG: " + a.getID();
+			text += "\nCargo: " + a.getCargo().getNome();
+			text += "\n-------------------------------------------------------------";
+		}
+		
+		return text;
+	}
 	
 
 }
