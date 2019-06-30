942b405d5a7aeb54a8d87b60f573acb15632b7fd
IsaacBernardes <51682756+IsaacBernardes@users.noreply.github.com>
2019-06-30 15:17:09 -0300

    Update by Isaac 30/06/2019 15:17
    -Classe remodelada para seguir a Arquitetura de Classes

--- dados/RepositorioFuncionario.java
+++ dados/RepositorioFuncionario.java
@@ -11,36 +11,21 @@ import negocio.Gestao;
 public class RepositorioFuncionario 
 {
 	private static ArrayList<Funcionario> Funcionarios = new ArrayList<Funcionario>();
-	Gestao gerencia = new Gestao();
+	//Gestao gerencia = new Gestao();
 	
-	public void adicionarFuncionario(String nome, int RG, Cargo cargo)
+	public ArrayList<Funcionario> getFuncionarios()
 	{
-		Funcionario novoF = new Funcionario(nome, RG, cargo);
-		boolean jaCadastrado = false;
-		
-		for(Funcionario a: Funcionarios)
-		{
-			if(novoF.equals(a))
-				jaCadastrado = true;
-		}
-		
-		if(!jaCadastrado)
-		{
-			Funcionarios.add(novoF);
-			gerencia.CadastrarFuncionario(novoF);
-			JOptionPane.showMessageDialog(null, "Funcionario cadastrado com sucesso", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
-			
-		}
-		
-		else
-		{
-			JOptionPane.showMessageDialog(null, "Funcionario já está cadastrado", "Error", JOptionPane.ERROR_MESSAGE);
-		}
+		return Funcionarios;
 	}
+	
+	public void adicionarFuncionario(Funcionario novo) {
+		Funcionarios.add(novo);
+	}
+	
 	
 	public void removerFuncionario(Funcionario funcionarioR)
 	{
-		boolean Cadastrado = false;
+		/*boolean Cadastrado = false;
 		Funcionario serRemovido = null;
 		
 		for(Funcionario a: Funcionarios)
@@ -59,44 +44,20 @@ public class RepositorioFuncionario
 		else
 		{
 			JOptionPane.showMessageDialog(null, "Funcionário não existe", "Error", JOptionPane.ERROR_MESSAGE);
-		}
+		}*/
 	}
 	
-	public Funcionario bucasFuncionario(String nomeB)
-	{
-		for(int i = 0; i < Funcionarios.size(); i++)
-		{
-			if(Funcionarios.get(i).getNome() == nomeB)
-			{
-				return Funcionarios.get(i);
-			}
-		}
-		
-		return null;
-	}
-	
-	public Funcionario buscaFuncionario(int RG)
-	{
-		for(Funcionario a: Funcionarios)
-		{
-			if(a.getID() == RG)
-			{
-				return a;
-			}
-		}
-		
-		return null;
-	}
 	
 	public String toString()
 	{
-		String text = "-------------------------------------------------------------";
+		String text = "";
 		
 		for(Funcionario a: Funcionarios) {
 			text += "\nNome: " + a.getNome();
 			text += "\nRG: " + a.getID();
 			text += "\nCargo: " + a.getCargo().getNome();
-			text += "\n-------------------------------------------------------------";
+			text += "\nSalario: R$" + a.getSalario();
+			text += "\n__________________________________________";
 		}
 		
 		return text;
