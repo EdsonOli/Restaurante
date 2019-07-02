236baacd18eac61cd2c1aa54d132c70740050783
marco052 <51726815+marco052@users.noreply.github.com>
2019-07-02 20:22:10 -0300

    Update by Marco (interface arquivos)

---
+++ dados/Arquivos.java
@@ -0,0 +1,10 @@
+package dados;
+
+public interface Arquivos
+{
+	//METODO PARA GRAVAR OBJETOS NO ARQUIVO
+	public void gravaLista();
+
+	//METODO PARA LER OBJETOS NO ARQUIVO
+	public void lerLista();
+}
