b6a517b17a16e2c683ad11415ecaa3c8cfec7eea
marco052 <51726815+marco052@users.noreply.github.com>
2019-07-02 20:30:30 -0300

    Update by Marco métodos de gravação e leitura

--- dados/RepositorioFuncionario.java
+++ dados/RepositorioFuncionario.java
@@ -1,14 +1,17 @@
 package dados;
 
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
-import bases.Funcionario;
-import bases.Cargo;
-import negocio.Gestao;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class RepositorioFuncionario 
+
+public class RepositorioFuncionario implements Serializable, Arquivos
 {
 	private static ArrayList<Funcionario> Funcionarios = new ArrayList<Funcionario>();
 	//Gestao gerencia = new Gestao();
@@ -18,14 +21,48 @@ public class RepositorioFuncionario
 		return Funcionarios;
 	}
 	
-	public void adicionarFuncionario(Funcionario novo) {
-		Funcionarios.add(novo);
+	public void adicionarFuncionario(String nome, int rg, Cargo cargo) {
+		Funcionarios.add(new Funcionario(nome, rg, cargo));
+	}
+
+	public void gravaLista(){
+		try{
+			FileOutputStream arqG2 = new FileOutputStream("RepositorioFuncionario.dat");
+			ObjectOutputStream objG2 = new ObjectOutputStream(arqG2);
+			objG2.writeObject(Funcionarios);
+			arqG2.flush();
+			objG2.flush();
+			arqG2.close();
+			objG2.close();
+		}
+
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public void lerLista(){		
+		try {
+			FileInputStream arqL2 = new FileInputStream("RepositorioFuncionario.dat");
+			ObjectInputStream objL2 = new ObjectInputStream(arqL2);
+			Funcionarios = (ArrayList<Funcionario>) objL2.readObject();
+			arqL2.close();
+			objL2.close();
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
+
+	//marco: nao vi essa parte aq pq for each n eh cmg
 	
-	public void removerFuncionario(Funcionario funcionarioR)
+	/*public void removerFuncionario(Funcionario funcionarioR)
 	{
-		/*boolean Cadastrado = false;
+		boolean Cadastrado = false;
 		Funcionario serRemovido = null;
 		
 		for(Funcionario a: Funcionarios)
@@ -44,8 +81,8 @@ public class RepositorioFuncionario
 		else
 		{
 			JOptionPane.showMessageDialog(null, "Funcionário não existe", "Error", JOptionPane.ERROR_MESSAGE);
-		}*/
-	}
+		}
+	}*/
 	
 	
 	public String toString()
