9db0b6d42849f60a4a93c07e52d9835163b69a58
Lclocker <47572247+Lclocker@users.noreply.github.com>
2019-07-10 15:26:46 -0300

    Update by Lucas (classe PratoPrincipal)

---
+++ PratoPrincipal.java
@@ -0,0 +1,17 @@
+package bases;
+
+import java.util.ArrayList;
+
+public class PratoPrincipal extends Prato{
+
+	public PratoPrincipal(String nome, double preco, ArrayList<Ingrediente> ingredientes) {
+		super(nome, preco, ingredientes);
+	}
+	
+	
+	//TODOS OS METODOS SERÃO IGUAS AOS DA SUPER CLASSE//
+	
+	
+	
+	
+}
