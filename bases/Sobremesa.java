63d4df48c4dabeffd27b4e6b55438821f4b11c30
Lclocker <47572247+Lclocker@users.noreply.github.com>
2019-07-10 15:41:31 -0300

    Update by Lucas (String porcao)

--- bases/Sobremesa.java
+++ bases/Sobremesa.java
@@ -4,30 +4,22 @@ import java.util.ArrayList;
 
 public class Sobremesa extends Prato{
 
-	private int porcao;
+	private String porcao;
 	
-	public Sobremesa(String nome, double preco, ArrayList<Ingrediente> ingredientes, int porcao) {
+	public Sobremesa(String nome, double preco, ArrayList<Ingrediente> ingredientes, String porcao) {
 		super(nome, preco, ingredientes);
 		this.porcao = porcao;
 	}
 	
 	
-	public int getPorcao(){
+	public String getPorcao(){
 		return this.porcao;
 	}
-	public void setPorcao(int quant) {
-		this.porcao = quant;
+	public void setPorcao(String porcao) {
+		this.porcao = porcao;
 	}
 	
 	//TODOS OS METODOS IGUAIS AOS DA SUPER CLASSE//
-	
-	public void aumentarPorcao(int quant) {
-		setPorcao(this.getPorcao() + quant);
-	}
-	public void diminuirPorcao(int quant) {
-		setPorcao(this.getPorcao() - quant);
-	}
-	
 	
 	//@override
 	public String toString() {
